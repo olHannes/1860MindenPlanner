1cfebb7c03b6f5ec0ab64a75555c2a36f26a81ff
HOlschok <holschok@gauselmann.de>
2025-03-19 11:42:20 +0100

    [userhandling] basic session creation (login / logout)

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,12 @@
 from flask import Flask
 from flask_cors import CORS
-
+import routes
 
 app = Flask(__name__)
-
+app.secret_key = 'ioddugjreotvu98vrv89t9<7077rvn9e8qw7rte6'
 CORS(app)
 
-from routes import main_bp
-app.register_blueprint(main_bp)
+app.register_blueprint(routes.main_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- backend/routes.py
+++ backend/routes.py
@@ -1,7 +1,24 @@
-from flask import Blueprint
+from flask import Blueprint, session, request, jsonify
 
 main_bp = Blueprint('main', __name__)
 
-@main_bp.route('/hello', methods=["GET"])
-def sayHello():
-    return "Hello World"
+# Route for user-login
+@main_bp.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    # validate User with Database
+
+    if 'user' in session:
+        return jsonify({"message": "User already logged in!"}), 400
+
+    session['user'] = username
+    return jsonify({"message": "Login successful!"}), 200
+
+# Route for user-logout
+@main_bp.route('/logout', methods=['POST'])
+def logout():
+    session.pop('user', None)
+    return jsonify({"message": "Logged out successfully!"}), 200
