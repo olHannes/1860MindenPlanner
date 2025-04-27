ce67913d4d173ed8223d812bdb462d8206583070
Hannes Olschok <hannes.o@t-online.de>
2025-04-27 12:32:41 +0200

    Bugfix - fixed server url

--- frontend/assets/script/code.js
+++ frontend/assets/script/code.js
@@ -1,6 +1,6 @@
 
 let serverURL = "https://one860mindenplanner.onrender.com";
-serverURL = "http://127.0.0.1:10000";
+//serverURL = "http://127.0.0.1:10000";
 
 window.onload = function () {
     try {
