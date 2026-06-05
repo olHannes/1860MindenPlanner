f09a15856f34b1442cec108caaba98b56a2cb41f
Hannes Olschok <hannes.o@t-online.de>
2026-06-05 13:02:27 +0200

    update Server-URL

--- frontend/assets/script/config.js
+++ frontend/assets/script/config.js
@@ -1,6 +1,5 @@
 export let serverURL = "https://one860mindenplanner.onrender.com";
-serverURL = "http://127.0.0.1:10000";
-//serverURL = "http://homeserver:10000";
+serverURL = "/api";
 
 export const APPARATUS = [
     {
